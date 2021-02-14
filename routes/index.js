0904318943314a5937505f513a638f3927a47a23
HongYeseul <ghd8119@naver.com>
2021-02-14 03:26:40 +0000

    First Commit - Create server with Express - Using Kakao searching Book API at '/'

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+var request = require('request');
+var express = require('express');
+var router = express.Router();
+
+const options = {
+  headers:{"Authorization": "KakaoAK KAKAO-REST-API-KEY"},
+  url:"https://dapi.kakao.com/v3/search/book",
+  qs:{
+    target: 'title',
+    query: '안녕',
+  },
+  json:true
+}
+
+/* GET home page. */
+router.get('/',function(req, res, next) {
+  request.get(options, function(err, response, body){
+    if(err)
+      res.send("ERROR");
+    else
+      res.json(body);
+  })
+  //res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
