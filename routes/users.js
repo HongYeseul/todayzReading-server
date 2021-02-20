213ae502c00d0bbc5f38f91359ee4f28b8b6cec6
HongYeseul <ghd8119@naver.com>
2021-02-20 03:53:02 +0000

    DB 서버와 연결

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.post('/login', function(req, res, next) {
+  try{
+
+  }catch(err){
+    
+  }
+});
+
+router.post('/signUp', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/doubleChk', function(req, res, next) {
   res.send('respond with a resource');
 });
 
